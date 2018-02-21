fef8988d647d3fbeba3cbeec400b102c641531a6
Hao Song of group cpsc424 <hs746@grace1.grace.hpc.yale.internal>
2018-02-21 12:36:10 -0500

    fix task1.c bug

--- Homework2/task1.c
+++ Homework2/task1.c
@@ -1,42 +1,36 @@
 #include <stdlib.h>
 #include <stdio.h>
-#include <complex.h>
 #include "drand.h"
 #include "timing.h"
 
 int main(int argc, char const *argv[]) {
     dsrand(12345);
-    // complex a = I;
-    // printf("%f\n", cabs(a));
     double wcs, wce, ct;
     timing(&wcs, &ct);
-    double x = -2.0, y = 0.0;
     int N0 = 0, N1 = 0;
-    for (double x = -2.0; x < 0.5; x += 0.001) {
-        for (double y = 0.0; y < 1.25; y += 0.001) {
-            double real = x + drand() * 0.001;
-            double imag = (y + drand() * 0.001) * I;
-            double complex zz = 1.0 + 1.0*I;
-            double complex c = real + imag;
-            double complex z = c;
-            printf("The conjugate of Z1 = %.2f %+.2fi\n", creal(c), cimag(c));
-            printf("abs of c = %.2f\n", cabs(c));
+    for (int x = -2000; x < 500; x++) {
+        for (int y = 0; y < 1250; y++) {
+            double cr = (drand() + (double)x) * 0.001;
+            double ci = (drand() + (double)y) * 0.001;
+            double zr = cr;
+            double zi = ci;
             int i = 0;
-            while (i++ < 20000) {
-                // double complex temp = z * z;
-                // temp = temp + c;
-                // if (cabs(c) > 2) {
-                    // N0++;
-                    // break;
-                // }
+            for (; i < 20000; i++) {
+                double new_zr = zr * zr - zi * zi + cr;
+                double new_zi = 2 * zr * zi + ci;
+                zr = new_zr;
+                zi = new_zi;
+                if (zr * zr + zi * zi > 4) break;
             }
             if (i == 20000) N1++;
+            else N0++;
         }
     }
     timing(&wce, &ct);
     double runtime = wce - wcs;
     double area = 2 * N1 * 3.125 / (N1 + N0);
-    printf("%f\n", area);
-    printf("%f\n", runtime);
+    printf("area = %f\n", area);
+    printf("runtime = %f\n", runtime);
     return 0;
 }
+
