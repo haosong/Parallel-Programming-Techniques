8ca5127f49f461934c5d0f2864b265172aea8bfd
Hao Song <songhao1119@gmail.com>
2018-04-10 01:33:35 -0400

    add blank lines

--- Homework3/task2.c
+++ Homework3/task2.c
@@ -11,6 +11,10 @@
 void block_matmul(double *A, double *B, double *C, int rowIndex, int colIndex, int blockSize, int N);
 
 int main(int argc, char **argv) {
+    printf("Matrix multiplication times:\n");
+    printf("   N      TIME (secs)    F-norm of Error\n");
+    printf(" -----   -------------  -----------------\n");
+
     int sizes[4]={1000,2000,4000,8000};
 
     char files[4][50]={"/home/fas/cpsc424/ahs3/assignment3/C-1000.dat",\
@@ -19,21 +23,18 @@ int main(int argc, char **argv) {
                        "/home/fas/cpsc424/ahs3/assignment3/C-8000.dat"};
     int rank, size;
 
-    printf("Matrix multiplication times:\n   N      TIME (secs)    F-norm of Error\n -----   -------------  -----------------\n");
-    fflush(stdout);
     MPI_Status status;
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-
     for (int run = 0; run < 4; run++) {
         double *A, *B, *C, *Ctrue;
-        int N = sizes[run];
-        int sizeAB, sizeC;
         double wcs, wce, ct;
         FILE *fptr;
+        int N = sizes[run];
         int blockSize = N / size;
+        int sizeAB, sizeC;
         sizeAB = N * (N + 1) / 2;
         sizeC = N * N;
 
@@ -45,6 +46,7 @@ int main(int argc, char **argv) {
             for (int i = 0; i < sizeAB; i++) A[i] = ((double) rand() / (double) RAND_MAX);
             for (int i = 0; i < sizeAB; i++) B[i] = ((double) rand() / (double) RAND_MAX);
             MPI_Barrier(MPI_COMM_WORLD);
+
             // Send the permanent row to all workers
             timing(&wcs, &ct);
             for (int i = 1; i < size; i++) {
@@ -52,32 +54,36 @@ int main(int argc, char **argv) {
                 int length = BLOCK_LEN(rowIndex, blockSize);
                 MPI_Send(A + rowIndex * (rowIndex + 1) / 2, length, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
             }
+
             // Send column to all workers
             for (int i = 1; i < size; i++) {
                 int colIndex = i * blockSize;
-                int length = BLOCK_LEN(colIndex, blockSize);;
+                int length = BLOCK_LEN(colIndex, blockSize);
                 MPI_Send(&colIndex, 1, MPI_INT, i, 1, MPI_COMM_WORLD);
                 MPI_Send(B + colIndex * (colIndex + 1) / 2, length, MPI_DOUBLE, i, 1, MPI_COMM_WORLD);
             }
+
             // Do the calculation on currant master node
             int rowIndex = 0;
             int colIndex = 0;
             double *col = B;
             block_matmul(A, col, C, rowIndex, colIndex, blockSize, N);
+
             // Send column to next node, receive column from prev node
             for (int i = 1; i < size; i++) {
                 MPI_Send(&colIndex, 1, MPI_INT, rank + 1, 1, MPI_COMM_WORLD);
                 MPI_Send(col, BLOCK_LEN(colIndex, blockSize), MPI_DOUBLE, rank + 1, 1, MPI_COMM_WORLD);
                 MPI_Recv(&colIndex, 1, MPI_INT, size - 1, 1, MPI_COMM_WORLD, &status);
                 MPI_Recv(col, BLOCK_LEN(colIndex, blockSize), MPI_DOUBLE, size - 1, 1, MPI_COMM_WORLD, &status);
-                // Calculate on those coming columns
                 block_matmul(A, col, C, rowIndex, colIndex, blockSize, N);
             }
             free(A);
             free(B);
+
             // Collect results from workers
             for (int i = 1; i < size; i++)
                 MPI_Recv(C + i * blockSize * N, blockSize * N, MPI_DOUBLE, i, 1, MPI_COMM_WORLD, &status);
+
             timing(&wce, &ct);
             Ctrue = (double *) calloc(sizeC, sizeof(double));
             fptr = fopen(files[run], "rb");
