d9be5f7ce7f44331f070ad3f28cf76cf61e7e907
Hao Song <songhao1119@gmail.com>
2018-04-22 00:32:52 -0400

    modify task1.cu

--- Homework4/task1.cu
+++ Homework4/task1.cu
@@ -5,34 +5,34 @@
 #include <stdlib.h>
 #include <math.h>
 
-__global__ void gpu_matrixmult(FP *a,FP *b, FP *c, int n) {
+__global__ void gpu_matrixmult(FP *a,FP *b, FP *c, int n, int m, int p) {
 
   int col = threadIdx.x + blockDim.x * blockIdx.x;
   int row = threadIdx.y + blockDim.y * blockIdx.y;
 
   int indexb = col;
-  int index = row * n + col;
+  int index = row * m + col;
   
-  if(col < n && row < n) {
+  if(col < m && row < n) {
     c[index] = 0.;
-    for (int indexa = row*n; indexa < (row*n + n); indexa++, indexb+=n) 
+    for (int indexa = row*p; indexa < (row*p + p); indexa++, indexb+=m) 
       c[index] += a[indexa]*b[indexb];
   }
 
 }
 
 
-void cpu_matrixmult(FP *a,FP *b, FP *c, int n) {
+void cpu_matrixmult(FP *a,FP *b, FP *c, int n, int m, int p) {
 
   int index, indexa, indexb;
   FP cvalue;
-  for(int col=0;col < n; col++)
+  for(int col=0;col < m; col++)
     for(int row=0;row < n; row++) {
       indexb = col;
-      index = row * n + col;
+      index = row * m + col;
       cvalue = 0.;
-      for (indexa = row*n; indexa < (row*n + n); indexa++, indexb+=n) 
-	cvalue += a[indexa]*b[indexb];
+      for (indexa = row*p; indexa < (row*p + p); indexa++, indexb+=m) 
+  cvalue += a[indexa]*b[indexb];
       c[index] -= cvalue; //NOTE: This calculates the diff between CPU and GPU computations.
     }
 }
@@ -44,10 +44,9 @@ int main(int argc, char *argv[]) {
 
   int gpucount = 0; // Count of available GPUs
   int gpunum = 0; // Device number to use
-  int Grid_Dim = 1; //Grid dimension, x and y, square
   int Block_Dim = 1; //Block dimension, x and y, square
 
-  int n; // matrix dimension
+  int n, m, p; // matrix dimension
   FP *a,*b,*c;
   FP *dev_a, *dev_b, *dev_c;
   int size; // number of bytes in arrays
@@ -67,42 +66,38 @@ int main(int argc, char *argv[]) {
      printf("Device count = %d\n",gpucount);
   }
 
-  if (argc!=4) {
-    printf("Usage: matmul <matrix dim> <block dim> <grid dim>\n");
+  if (argc!=6) {
+    printf("Usage: matmul <matrix dim n> <matrix dim m> <matrix dim p> <block dim>\n");
     exit (-1);
   }
 
   n = atoi(argv[1]);
+  m = atoi(argv[2]);
+  p = atoi(argv[3]);
 
-  Block_Dim = atoi(argv[2]); // Square block
+  Block_Dim = atoi(argv[4]); // Square block
   if (Block_Dim*Block_Dim > 1024) {
     printf("Error, too many threads in block\n");
     exit (-1);
   }
-
-  Grid_Dim = atoi(argv[3]); // Square grid
-  if (Grid_Dim*Block_Dim < n) {
-    printf("Error, number of threads in x/y dimensions less than number of array elements\n");
-    exit (-1);
-  }
+  Grid_Dim_m = (m - 1) / Block_Dim + 1;
+  Grid_Dim_n = (n - 1) / Block_Dim + 1;
 
   cudaSetDevice(gpunum);
   printf("Using device %d\n",gpunum);
   
   printf("Matrix Dimension = %d\n",n);
-  printf("Block_Dim = %d, Grid_Dim = %d\n",Block_Dim,Grid_Dim);
+  printf("Block_Dim = %d, Grid_Dim[m, n] = [%d, %d]\n",Block_Dim,Grid_Dim_m,Grid_Dim_n);
 
-  dim3 Grid(Grid_Dim, Grid_Dim); //Grid structure
+  dim3 Grid(Grid_Dim_m, Grid_Dim_n); //Grid structure
   dim3 Block(Block_Dim, Block_Dim); //Block structure
 
-  size = n * n * sizeof(FP); // number of bytes in total in arrays
-
-  a = (FP*) malloc(size); // dynamically allocated memory for arrays on host
-  b = (FP*) malloc(size);
-  c = (FP*) malloc(size); // results from GPU
+  a = (FP *) malloc(n * p * sizeof(FP)); // dynamically allocated memory for arrays on host
+  b = (FP *) malloc(p * m * sizeof(FP));
+  c = (FP *) malloc(n * m * sizeof(FP)); // results from GPU
 
   srand(12345);
-  int p = n; //Used here only to illustrate proper initialization for non-square case
+  // int p = n; //Used here only to illustrate proper initialization for non-square case
   for(i=0;i < n;i++)
     for(j=0;j < p;j++) {
       a[i * p + j] = (FP) rand() / (FP) RAND_MAX;
@@ -110,8 +105,8 @@ int main(int argc, char *argv[]) {
     }
 
   for(i=0;i < p;i++)
-    for(j=0;j < n;j++) {
-      b[i * n + j] = (FP) rand() / (FP) RAND_MAX;
+    for(j=0;j < m;j++) {
+      b[i * m + j] = (FP) rand() / (FP) RAND_MAX;
       //      b[i * n + j] = (FP) i+j; // may be helpful for debugging
     }
 
